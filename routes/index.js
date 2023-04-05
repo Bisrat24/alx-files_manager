09b72ec8a2a16a72f770fffef0da2f8975a01344
root <root@DESKTOP-738JLEQ.localdomain>
2023-04-05 23:40:00 +0300

    files_manager2

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,33 @@
-import express from 'express';
+import { Router } from 'express';
 import AppController from '../controllers/AppController';
+import UsersController from '../controllers/UsersController';
+import AuthController from '../controllers/AuthController';
+import FilesController from '../controllers/FilesController';
 
-const app = express();
+const router = Router();
 
-app.get('/', (req, res) => { res.send('Hello World!\n'); });
+router.get('/status', AppController.getStatus);
 
-app.get('/status', (req, res) => { res.send(AppController.getStatus()); });
+router.get('/stats', AppController.getStats);
 
-app.get('/stats', (req, res) => { res.send(AppController.getStats()); });
+router.post('/users', UsersController.postNew);
 
-module.exports = app;
+router.get('/connect', AuthController.getConnect);
+
+router.get('/disconnect', AuthController.getDisconnect);
+
+router.get('/users/me', UsersController.getMe);
+
+router.post('/files', FilesController.postUpload);
+
+router.get('/files/:id', FilesController.getShow);
+
+router.get('/files', FilesController.getIndex);
+
+router.put('/files/:id/publish', FilesController.putPublish);
+
+router.put('/files/:id/unpublish', FilesController.putUnpublish);
+
+router.get('/files/:id/data', FilesController.getFile);
+
+module.exports = router;
--- utils/db.js
+++ utils/db.js
@@ -1,29 +1,37 @@
-import MongoClient from 'mongodb';
+import { MongoClient } from 'mongodb';
+
+const HOST = process.env.DB_HOST || 'localhost';
+const PORT = process.env.DB_PORT || 27017;
+const DATABASE = process.env.DB_DATABASE || 'files_manager';
+
+const url = `mongodb://${HOST}:${PORT}`;
 
 class DBClient {
   constructor() {
-    const host = process.env.DB_HOST || 'localhost';
-    const port = process.env.DB_PORT || 27017;
-    const database = process.env.DB_DATABASE || 'files_manager';
-    const url = `mongodb://${host}:${port}`;
-    MongoClient.connect(url, { useUnifiedTopology: true }, (error, client) => {
-      this.db = client.db(database);
-      this.users = this.db.collection('users');
-      this.files = this.db.collection('files');
+    this.client = new MongoClient(url, { useUnifiedTopology: true, useNewUrlParser: true });
+    this.client.connect().then(() => {
+      this.db = this.client.db(`${DATABASE}`);
+    }).catch((err) => {
+      console.log(err);
     });
   }
 
   isAlive() {
-    return Boolean(this.db);
+    return this.client.isConnected();
   }
 
   async nbUsers() {
-    return this.users.countDocuments();
+    const users = this.db.collection('users');
+    const usersNum = await users.countDocuments();
+    return usersNum;
   }
 
   async nbFiles() {
-    return this.files.countDocuments();
+    const files = this.db.collection('files');
+    const filesNum = await files.countDocuments();
+    return filesNum;
   }
 }
+
 const dbClient = new DBClient();
-export default dbClient;
+module.exports = dbClient;
